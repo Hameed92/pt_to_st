ba06fbc98b51b9d8b277255129c40f20cb5fb60c
Nicolas Patry <Narsil@users.noreply.huggingface.co>
2022-11-26 20:46:01 +0000

    Update convert.py

--- convert.py
+++ convert.py
@@ -95,7 +95,7 @@ def convert_file(
     pt_filename: str,
     sf_filename: str,
 ):
-    loaded = torch.load(pt_filename)
+    loaded = torch.load(pt_filename, map_location="cpu")
     shared = shared_pointers(loaded)
     for shared_weights in shared:
         for name in shared_weights[1:]:
